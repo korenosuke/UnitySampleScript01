676f864b6f4a33b0d26304a4af2d4ac890cfaa8e
OriishiTakahiro <takahiro0914@live.jp>
2016-06-26 00:37:32 +0900

    fixed ver

--- オペレーションメテオ/Assets/Resources/Script/ResultViewController.cs
+++ オペレーションメテオ/Assets/Resources/Script/ResultViewController.cs
@@ -26,9 +26,13 @@ public class ResultViewController : MonoBehaviour {
 		if(timer.IsTimeOver()) {
 			text.text = "GameClear !!";
 			text.color = new Color(0f, 1f, 0f, 1f);
+			foreach(GameObject obj in GameObject.FindGameObjectsWithTag("meteo"))
+				Destroy(obj);
 		}	else if(player.IsGameOver()) {
 			text.text = "GameOver !!";
 			text.color = new Color(1f, 0f, 0f, 1f);
+			foreach(GameObject obj in GameObject.FindGameObjectsWithTag("meteo"))
+				Destroy(obj);
 		}
 	}
 }
